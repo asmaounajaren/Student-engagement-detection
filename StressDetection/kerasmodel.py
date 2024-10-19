55fac17dcf3fd3ea6f56595a96ffefbcda0ba7a9
codingirlma <asmaounajaren@gmail.com>
2024-10-19 01:11:53 +0100

    first commit

---
+++ StressDetection/kerasmodel.py
@@ -0,0 +1,116 @@
+import numpy as np
+import argparse
+import cv2
+import os
+import matplotlib as mpl
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+
+# command line argument
+# Command line argument
+ap = argparse.ArgumentParser()
+ap.add_argument("--mode", help="train/display")
+args = ap.parse_args()
+mode = args.mode
+
+
+
+# Define data generators
+train_dir = 'data/train'
+val_dir = 'data/test'
+
+num_train = 2986
+num_val = 908
+batch_size = 14
+num_epoch = 50
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(48,48),
+        batch_size=batch_size,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+validation_generator = val_datagen.flow_from_directory(
+        val_dir,
+        target_size=(48,48),
+        batch_size=batch_size,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+# Create the model
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
+
+# If you want to train the same model or try other models, go for this
+if mode == "train":
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=Adam(lr=0.0001, decay=1e-6),
+                  metrics=['accuracy'])
+
+    #plot_model_history(model_info)
+    model.save('model.h5')
+
+# emotions will be displayed on your face from the webcam feed
+elif mode == "display":
+    model = load_model('model.h5')
+
+    # prevents openCL usage and unnecessary logging messages
+    cv2.ocl.setUseOpenCL(False)
+
+    # dictionary which assigns each label an emotion (alphabetical order)
+    emotion_dict = {0: "Not_Engaged_Confused", 1: "Not_Engaged_Not_Liked", 2: "Engaged_Paying_Attention", 3: "Not_Engaged_Chatting", 4: "Engaged_Paying_Attention", 5: "Engaged_Making_Involvement", 6: "Engaged_Likes_Topic"}
+
+    # start the webcam feed
+    cap = cv2.VideoCapture(0)
+    while True:
+        # Find haar cascade to draw bounding box around face
+        ret, frame = cap.read()
+        facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+            prediction = model.predict(cropped_img)
+            maxindex = int(np.argmax(prediction))
+            cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+        # show the output frame
+        cv2.imshow("Alex Corporations Press Q to Exit", frame)
+        key = cv2.waitKey(1) & 0xFF
+        
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
